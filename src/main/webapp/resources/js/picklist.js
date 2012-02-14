03f4f9520f8e9fba1113f40eb996f623ede458eb
Brian Leathem <bleathem@gmail.com>
2012-02-13 21:05:55 -0800

    Removed support for multi-drag

--- src/main/webapp/resources/js/picklist.js
+++ src/main/webapp/resources/js/picklist.js
@@ -11,87 +11,16 @@
                 var picklist = $(this);
                 picklist.hasClass("pickList") || picklist.addClass("pickList");
                 var lists = picklist.children("ol");
-                var listLeft = $(lists.get(0));
-                var listRight = $(lists.get(1));
-                listLeft.hasClass("left") || listLeft.addClass("left");
-                listRight.hasClass("right") || listRight.addClass("right");
                 var selected = $([]), offset = {top:0, left:0};
-                lists.sortable({connectWith:lists, handle:".handle",
-                    start:function (ev, ui) {
-                        var dragItem = ui.item;
-                        var dragItemElement = ui.item.get(0);
-                        var dy = -40;
-                        if (ui.item.hasClass("ui-selected")) {
-                            selected = $(".ui-selected").not(dragItemElement).not(".ui-sortable-placeholder").each(function () {
-                                var item = $(this);
-                                item.data("offset", item.offset());
-                                item.css({
-                                    position: "relative"
-                                });
-                            }).detach();
-                        }
-                        else {
-                            selected = $([]);
-                            $("#selectable > div").removeClass("ui-selected");
-                        }
-                        offset = dragItem.offset();
-                    },
-                    change: function (ev, ui) {
-//                        console.log(ui);
-                    },
-                    sort:function (ev, ui) {
-                        var dragItem = ui.item;
-                        var dragItemElement = ui.item.get(0);
-                        var ref_position = ui.item.position();
-                        var dt = ref_position.top - offset.top;
-                        var dl = ref_position.left - offset.left;
-
-                        selected.not(dragItemElement).not(".ui-sortable-placeholder").each(function () {
-                            var item = $(this), off = item.data("offset");
-                            item.css({top:dt, left:dl});
-                        });
-                    },
-                    update: function(event, ui) {
-                        var dragItem = ui.item;
-                        var dragItemElement = ui.item.get(0);
-                        var newParent = $(dragItemElement.parentElement);
-                        selected.not(dragItemElement).not(".ui-sortable-placeholder").detach().insertAfter(dragItem);
-                        selected.not(dragItemElement).each(function () {
-                            $(this).css({top:0, left:0});
-                        });
-                        newParent.sortable("refreshPositions");
-
-                    },
-                    remove:function (event, ui) {
-                        var dragItem = ui.item;
-                        var dragItemElement = ui.item.get(0);
-                        var newParent = $(dragItemElement.parentElement);
-                        selected.not(dragItemElement).not(".ui-sortable-placeholder").detach().insertAfter(dragItem);
-                        selected.not(dragItemElement).each(function () {
-                            $(this).css({top:0, left:0});
-                        });
-                        newParent.sortable("refreshPositions");
-                    },
-                    beforeStop:function (event, ui) {
-                    },
-                    stop:function (event, ui) {
-                    }
-
-                });
+                lists.sortable({connectWith:lists, handle:".handle"});
                 lists.selectable().children("li").prepend("<div class='handle'><span class='ui-icon ui-icon-carat-2-n-s'></span></div>");
-                listLeft.after(createButtons(settings));
-                $(".buttons .right", picklist).click(function () {
-                    var elements = $(".ui-selected", listLeft).detach().appendTo(listRight);
-                });
-                $(".buttons .left", picklist).click(function () {
-                    var elements = $(".ui-selected", listRight).detach().appendTo(listLeft);
-                });
+                createButtons(picklist, lists, settings);
             })
         }
 
     };
 
-    var createButtons = function (settings) {
+    var createButtons = function (picklist, lists, settings) {
         var container = $("<ul />", {
             class:"buttons"
         });
@@ -100,15 +29,21 @@
         var buttonLeft = $("<button />", {
             class:"left",
             html:settings.leftButtonText
-        }).button().appendTo(itemLeft);
+        }).button().click(function () {
+            var elements = $(".ui-selected", lists[0]).detach().appendTo(lists[1]);
+        }).appendTo(itemLeft);
 
         var itemRight = $("<li />").appendTo(container);
         var buttonRight = $("<button />", {
             class:"right",
             html:settings.rightButtonText
-        }).button().appendTo(itemRight);
+        }).button().click(function () {
+            var elements = $(".ui-selected", lists[1]).detach().appendTo(lists[0]);
+        }).appendTo(itemRight);
 
-        return container;
+        $(lists[0]).after(container);
+
+        return picklist;
     }
 
     $.fn.picklist = function (method) {
